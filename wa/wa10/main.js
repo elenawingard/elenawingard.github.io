7551c7facc878b31fa21a420fab8636b918c231e
elenawingard <elwi4382@colorado.edu>
2023-11-02 17:07:02 -0600

    updates

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -8,10 +8,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When she got to :inserty:, she stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-var insertX = ["Elena","Elizabeth","Connor"];
-var insertY = ["the Boilder Room","prison","their friend's house"];
+var insertX = ["Amanda","Isabelle","Emma"];
+var insertY = ["the Boiler Room","prison","her friend's house"];
 var insertZ = ["committed a crime","started hallucinating","called the cops"];
 
 
