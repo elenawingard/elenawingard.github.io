e5ee2d8233a578314eb6e147883f6b22839a53d2
elenawingard <elwi4382@colorado.edu>
2023-11-28 13:29:08 -0700

    updates

--- js-final/final.js
+++ js-final/final.js
@@ -1,5 +1,9 @@
 // other idea: every time someone tries to type a number x, have it return x + 1, etc.
 
+
+// maybe search up how to make a spinning thing that returns an answer
+
+
 const newBtn = document.querySelector('#js-new-button').addEventListener('click', getNumber);
 
 const canvas = document.querySelector('canvas');
@@ -8,6 +12,28 @@ const ctx = canvas.getContext('2d');
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+// this will be the thing that spins 
+// needs to spin clockwise...
+// ...fast for 2 secs, slower for 1.5 secs, then it slows down...
+// ... til it stops and reveals the number
 class wheel {
+    constructor(x, y, velX, velY, color, size) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+        ctx.fill();
+    }
+
+  //  update() { ????
 
 }
+
