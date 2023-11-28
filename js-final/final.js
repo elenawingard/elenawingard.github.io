b0b60db92364344a5e7262673d40b474d415786a
elenawingard <elwi4382@colorado.edu>
2023-11-28 12:03:06 -0700

    create

---
+++ js-final/final.js
@@ -0,0 +1,13 @@
+// other idea: every time someone tries to type a number x, have it return x + 1, etc.
+
+const newBtn = document.querySelector('#js-new-button').addEventListener('click', getNumber);
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+class wheel {
+
+}
